4ccdce0f9ce9ae4aa7c3752025f02e38a4445457
JoanFrago <jfragojimenez@gmail.com>
2024-03-21 10:58:54 +0100

    video and audio downloader

---
+++ audioDownloader.py
@@ -0,0 +1,37 @@
+# https://youtu.be/SICEtxAc1i4?si=Dxiqp3KxI-vVZvL6
+
+from pytube import YouTube
+import os
+
+os.system("clear")
+
+def Download(video):
+  print("Downloading...")
+  video.download()
+  print("Audio Downloaded Successfully")
+
+def ConfirmDownload(video):
+  is_download = str(input("\n Are you sure you want to download it? (Y / N): ").lower())
+  if is_download == "y":
+    Download(video)
+  elif is_download == "n":
+    exit()
+  else:
+    print("not a correct value, try again")
+
+def InfoVideo(video):
+  print(f"\n Title \n {video.title} \n\n File Size \n {video.filesize / 1000000} MB")
+  ConfirmDownload(video)
+
+def FindVideo(video_url):
+  try:
+    video = YouTube(video_url)
+    video = video.streams.get_audio_only()
+  except:
+    print("URL not valid")
+    exit()
+  
+  InfoVideo(video)
+
+video_url = str(input(("Type in the URL of the video you want to download the audio from: \n --> ")))
+FindVideo(video_url)
