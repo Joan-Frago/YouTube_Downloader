ec0751b5ad1d882ea8bb5fe2b1bb1e6b9255c5a9
JoanFrago <jfragojimenez@gmail.com>
2024-03-22 09:56:26 +0100

    Finished selector

--- selector.py
+++ selector.py
@@ -6,18 +6,15 @@ import os
 os.system("clear")
 
 def select():
-    try:
-        option = int(input("\n 1.    Download Video\n 2.    Download Audio\n\n --> "))
-        if option == 1:
-            # call Download Video function
-            videoDownloader.FindVideo()
-        elif option == 2:
-            #call Download Audio function
-            audioDownloader.FindVideo()
-        else:
-            print("Not an option")
-    except:
-        print("Type a number")
+    option = int(input("\n 1.    Download Video\n 2.    Download Audio\n\n --> "))
+    if option == 1:
+        # call Download Video function
+        videoDownloader.FindVideo()
+    elif option == 2:
+        #call Download Audio function
+        audioDownloader.FindVideo()
+    else:
+        print("Not an option")
         select()
 
 select()
