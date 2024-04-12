7d7a6069e8008cd362197076b093a2da4e5745fa
Joan-Frago <jfragojimenez@gmail.com>
2024-04-12 09:05:57 +0200

    trying pyscript

---
+++ main.py
@@ -0,0 +1,10 @@
+import audioDownloader
+import videoDownloader
+
+from pyscript import document
+
+def downloadVideo():
+    videoDownloader.FindVideo()
+
+def downloadAudio():
+    audioDownloader.FindVideo()
