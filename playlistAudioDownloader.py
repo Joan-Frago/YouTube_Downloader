0c6423c5b5a492ee8276d078fe474954f34217cb
Joan-Frago <150782224+Joan-Frago@users.noreply.github.com>
2024-05-07 12:11:00 +0200

    adding playlist integration, not working at the moment

---
+++ playlistAudioDownloader.py
@@ -0,0 +1,32 @@
+from pytube import Playlist
+import os
+
+def Download(playlist):
+    print("Downloading...")
+    for video in playlist.videos:
+        video.streams.get_audio_only().download(output_path = "/Users/joanpersonal/Documents/Personal/YTDownloader/Playlists/")
+    print("Playlist Downloaded Successfully")
+
+def ConfirmDownload(playlist):
+    is_download = str(input("\n Are you sure you want to download it? (Y / N): ").lower())
+    if is_download == "y":
+        Download(playlist)
+    elif is_download == "n":
+        exit()
+    else:
+        print("not a correct value, try again")
+
+def InfoPlaylist(playlist):
+    print(f"\n Title \n {playlist.title} \n\n File Size \n {playlist.filesize / 1000000} MB")
+    ConfirmDownload(playlist)
+
+def FindPlaylist():
+    os.system("clear")
+    playlist_url = str(input(("Type in the URL of the playlist you want to download: \n --> ")))
+    try:
+        playlist = Playlist(playlist_url)
+    except:
+        print("URL not valid")
+        exit()
+  
+    InfoPlaylist(playlist)
--- selector.py
+++ selector.py
@@ -1,18 +1,25 @@
-# https://youtu.be/SICEtxAc1i4?si=Dxiqp3KxI-vVZvL6
+# Video example --> https://youtu.be/SICEtxAc1i4?si=Dxiqp3KxI-vVZvL6
+# Playlist example --> https://www.youtube.com/playlist?list=PLthLmPLEszwHBOQab2_jvY9_fbvaBBopJ
 
 import audioDownloader
 import videoDownloader
+import playlistVideoDownloader
+import playlistAudioDownloader
 import os
 os.system("clear")
 
 def select():
-    option = int(input("\n 1.    Download Video\n 2.    Download Audio\n\n --> "))
+    option = int(input("\n 1.    Download Video\n 2.    Download Audio\n 3.    Download Video Playlist\n 4.    Download Audio Playlist\n\n --> "))
     if option == 1:
         # call Download Video function
         videoDownloader.FindVideo()
     elif option == 2:
         #call Download Audio function
         audioDownloader.FindVideo()
+    elif option == 3:
+        playlistVideoDownloader.FindPlaylist()
+    elif option == 4:
+        playlistAudioDownloader.FindPlaylist()
     else:
         print("Not an option")
         select()
